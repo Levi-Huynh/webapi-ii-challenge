602dbe3e5da1924941be9f8cfd99575c552f953c
Levi-Huynh <Levi_huynh@yahoo.com>
2019-05-14 14:28:30 -0700

    corrected server js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 
   const postsRouter = require('./posts/posts-router.js');
 
+  const db = require('./posts/db.js');
+
 const server = express();
 
 server.use(express.json());
@@ -12,12 +14,24 @@ server.use(cors());
 
 server.get('/', async (req, res) => {
   try {
-      const shoutouts = await postsRouter('shoutouts');
+       const shoutouts = await db('shoutouts');
     res.status(200).json({messageOfTheDay: process.env.MOTD, shoutouts
   });
   } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
+  }
+});
+
+server.post('/', async (req, res) => {
+  try {
+    const [id] = await db('shoutouts').insert(req.body);
+    const shoutouts = await db('shoutouts');
+
+    res.status(201).json(shoutouts);
+  } catch (error) {
+    console.error('\nERROR', error);
+    res.status(500).json({ error: 'Cannot add the shoutout' });
   }
 });
 
